370dbcc4059364b096e35091927b8002ee7aff26
mateossio <mateofbossio@gmail.com>
2023-08-30 11:56:56 -0300

    Added imgs for better understand redirection

--- green.tf
+++ green.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "green" {
   subnet_id              = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
   vpc_security_group_ids = [module.app_security_group.security_group_id]
   user_data = templatefile("${path.module}/init-script.sh", {
-    file_content = "version 1.1 - #${count.index}"
+    file_content = "g${count.index}"
   })
 
   tags = {
